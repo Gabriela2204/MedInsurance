3fa9dc5fa3551308826781ce7a8e9d3d2a2b84bf
test <gabriela.postelnicescu02@e-uvt.ro>
2022-10-19 15:00:05 +0300

    Changed DatabaseConnection and Constants

--- DatabaseConnection.php
+++ DatabaseConnection.php
@@ -1,34 +1,43 @@
 <?php
  
-require('Constants.php');
 
 class DatabaseConnection{
    
    private static $instance;
    private static $dbh ;
-
-
-   public static function getInstance()
-   {
-      if(!isset(self::$instance)) {
-        self::$instance = new static();
-     try{   
- 
-      $dbh = new \PDO('mysql:host='.localhost.';dbname='.dbname, user, pass);
-      return $dbh;
-        }catch(\Exception $e){
-           echo $e->getMessage();
-        }
-      
-   }
-   else{
-      return self::$dbh;
-   }
-}
-
   
+   private function __construct(){
+      
+      $const = json_decode(file_get_contents('Constants.json'), true);
+      self::$dbh = new \PDO('mysql:host='.$const['localhost'].';dbname='.$const['dbname'],
+      $const['user'], $const['pass']);
+      
+       
+   }
+
+   public function __clone(){
+      throw new Exception("Can't clone");
+   }
+
+   public function __wakeup()
+   {
+      throw new Exception("Can't unserialize ");
+   }
+
+   public function __serialize()
+   {
+      throw new Exception("Can't serialize ");
+   }
+
+   public static function getInstance(){
+      if(!isset(self::$instance)) {
+        self::$instance = new static(); 
+
+      }
+      return self::$dbh;
+   
+   }
 
 }
-
 
 ?>
