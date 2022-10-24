21f587276c0921f20fdad4f10ce931120ccf9f56
test <gabriela.postelnicescu02@e-uvt.ro>
2022-10-24 12:21:01 +0300

    Modified Configuration file and added changes to BaseRepository file

--- src/Configuration.php
+++ src/Configuration.php
@@ -5,41 +5,15 @@ namespace App;
 
 class Configuration{
 
-    private $localhost;
-    private $user;
-    private $dbname;
-    private $pass;
 
-
-    public function __construct()
+    public static function getConstantByKey($key)
     {
         $const = json_decode(file_get_contents('src\Constants.json'), true);
-        $this->localhost = $const['localhost'];
-        $this->user = $const['user'];
-        $this->dbname = $const['dbname'];
-        $this->pass = $const['pass'];
+         return $const[$key];
+        
     }
 
-    public function getLocalhost(){
-  
-        return $this->localhost; 
 
-    }
-    public function getUser(){
-  
-        return $this->user; 
-
-    }
-    public function getDbname(){
-  
-        return $this->dbname; 
-
-    }
-    public function getPass(){
-  
-        return $this->pass; 
-
-    }
 }
 
 ?>
--- src/Repository/BaseRepository.php
+++ src/Repository/BaseRepository.php
@@ -2,35 +2,66 @@
 
 namespace App\Repository;
 use  App\DatabaseConnection;
+use ReflectionClass;
 
-trait BaseRepository{
+class BaseRepository{
+
+    private $tableName;
+
+    public function __construct()
+    {
+        $this->tableName = strtolower(ltrim(get_called_class(),"App\Repository"));
+    }
 
     public function queryAndFetch($sql)
     {
+
         $db = DatabaseConnection::getInstance();
         if($db != null)
-       { $result = $db->prepare($sql);
+       { 
+        $result = $db->prepare($sql);
         $result -> execute();
-        $response= $result->fetchAll(\PDO::FETCH_ASSOC);
-        return $response;}
-
-        
+        $response= $result->fetchAll(\PDO::FETCH_CLASS,"App\Entity\\".ucfirst($this->tableName));
+        return $response;
+    }
+ 
     }
 
-    public function findAll($table){
-        return $this->queryAndFetch('select * from '.$table);
+    public function getClassProperties(){
+        $reflect = new ReflectionClass("App\Entity\\".ucfirst($this->tableName));
+        $props = $reflect->getProperties();
+        $string="";
+        foreach ($props as $prop) {
+            $string = $string.$prop->getName().',';
+        }
+        
+        return rtrim($string,',');
+    }
+
+    public function findAll(){
+        
+        return $this->queryAndFetch('select * from '.$this->tableName);
  
      }
 
-     public function find($id, $table){
+     public function find($id){
 
-        return $this->queryAndFetch('select * from '.$table.'where id='.$id);
+        return $this->queryAndFetch('select * from '.$this->tableName.'where id=:'.$id);
     }
 
-    public function delete($id, $table){
+    public function delete($id){
        
         
-        return $this->queryAndFetch('delete from '.$table.' where id='.$id);
+        return $this->queryAndFetch('delete from '.$this->tableName.' where id=:'.$id);
+    }
+
+
+    public function insert($object){
+
+        $properties = $this->getClassProperties();
+        echo $properties;
+        
+        // return $this->queryAndFetch('insert into'.$this->tableName.'('.$string.') values ('.$object->getId().','.$object->getId_Type().')');
     }
 }
 
